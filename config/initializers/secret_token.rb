8224a6f4dd8c9ebf9fbcda1e76a76218f0f1d335
Stewart <mr@man.com>
2010-08-20 17:30:56 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Telescope::Application.config.secret_token = 'f32dea6d3ffe49f4cb458c8215c59323837fd31a293bf901a5f9627a4a9de1078abbe50a7c65392c0d54284d76875d44bfe78c3de70b4516f9b9546050220218'
