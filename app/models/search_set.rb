caba6cf0fb5a49d5f91f71cdf55af26d8b11284d
Stewart <mr@man.com>
2010-09-07 15:01:54 +0100

    Changed search set mode

--- app/models/search_set.rb
+++ app/models/search_set.rb
@@ -2,9 +2,8 @@ class SearchSet < ActiveRecord::Base
   belongs_to :user, :class_name => "User", :foreign_key => "user_id"
   validates_presence_of :search_query_string, :on => :create, :message => "Can't have an empty search"
   
-  def topics(options = {})
-    return Post.latest_first.search search_query_string if options.empty?
-    Post.latest_first.search search_query_string, options
+  def topics
+    Post.latest_first.search search_query_string, :match_mode => :extended
   end
   
 end
